4ed47a7904309258a87cdf5f4796b6b9987f20a3
GorbachL <52042279+GorbachL@users.noreply.github.com>
2019-06-25 13:19:49 +0300

    Update CitiesDaoImpl.java

--- transportCompany/src/main/java/dao/CitiesDaoImpl.java
+++ transportCompany/src/main/java/dao/CitiesDaoImpl.java
@@ -15,10 +15,26 @@ public class CitiesDaoImpl implements CitiesDao {
     public City getCityByName(String name) {
         List<City> cityList = new ArrayList<>();
         try (Connection connection = MySQLConnection.getConnection()) {
-            Statement statement = connection.prepareStatement(SELECT);
-            ((PreparedStatement) statement).setString(1, name);
-            ResultSet result = statement.executeQuery(SELECT);
-            while (result.next()) {
+            PreparedStatement statement = connection.prepareStatement(SELECT);
+            statement.setString(1, name);
+            cityList = resultSetToList(statement.executeQuery());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //return cityList.size() == 0 ? new City() : cityList.get(0);
+
+        if (cityList.size() == 0 ){
+            return new City();
+        }else {
+            return cityList.get(0);
+        }
+    }
+
+    private List<City> resultSetToList(ResultSet result){
+        List<City> cityList = new ArrayList<>();
+        try {
+            while (result.next()){
                 City city = new City();
                 city.setId(result.getInt("id"));
                 city.setName(result.getString("name"));
@@ -31,7 +47,31 @@ public class CitiesDaoImpl implements CitiesDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return cityList.size() == 0 ? new City() : cityList.get(0);
+        return cityList;
     }
+}
+//     @Override
+//     public City getCityByName(String name) {
+//         List<City> cityList = new ArrayList<>();
+//         try (Connection connection = MySQLConnection.getConnection()) {
+//             Statement statement = connection.prepareStatement(SELECT);
+//             ((PreparedStatement) statement).setString(1, name);
+//             ResultSet result = statement.executeQuery(SELECT);
+//             while (result.next()) {
+//                 City city = new City();
+//                 city.setId(result.getInt("id"));
+//                 city.setName(result.getString("name"));
+//                 city.setLat(result.getDouble("latitude"));
+//                 city.setLon(result.getDouble("longitude"));
+//                 city.setAirport(result.getBoolean("airport"));
+//                 city.setSeaport(result.getBoolean("seaport"));
+//                 cityList.add(city);
+//             }
+//         } catch (SQLException e) {
+//             e.printStackTrace();
+//         }
+//         return cityList.size() == 0 ? new City() : cityList.get(0);
+//     }
+    
 }
 
