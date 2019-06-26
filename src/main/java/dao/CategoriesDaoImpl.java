ad1307dbad9d7b1aed98c89f0bf7dd20490843cc
GorbachL <52042279+GorbachL@users.noreply.github.com>
2019-06-26 12:59:29 +0300

    new commit

---
+++ src/main/java/dao/CategoriesDaoImpl.java
@@ -0,0 +1,62 @@
+package dao;
+
+import db.MySQLConnection;
+import entity.Category;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesDaoImpl implements CategoriesDao {
+
+    private static final String SELECT_ALL = "SELECT * FROM categories";
+    private static final String FIND_BY_NAME = "select * from categories where name = ? ";
+
+    @Override
+    public List<Category> get() {
+        List<Category> categories = new ArrayList<>();
+        try (Connection connection = MySQLConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(SELECT_ALL);
+            while (result.next()) {
+                Category category = new Category();
+                category.setId(result.getInt("id"));
+                category.setName(result.getString("name"));
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
+
+    @Override
+    public Category findByName(String name) {
+        List<Category> categories = new ArrayList<>();
+        try (Connection connection = MySQLConnection.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_BY_NAME);
+            statement.setString(1, name);
+            categories = resultSetToList(statement.executeQuery());
+             //TODO: call FIND_BY_NAME query
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories.get(0);
+    }
+
+    private List<Category> resultSetToList(ResultSet result){
+        List<Category> categories = new ArrayList<>();
+        try{
+            while (result.next()){
+                Category category = new Category();
+                category.setId(result.getInt("id"));
+                category.setName(result.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
+}
+
+
