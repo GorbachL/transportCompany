ec91ea9fc82d809a1a155f2fce9e04a49b209a15
GorbachL <52042279+GorbachL@users.noreply.github.com>
2019-06-26 10:28:14 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import controllers.CitiesController;
+import controllers.TransportController;
+import entity.City;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+//        CategoryController categoryController = new CategoryController();
+//        System.out.println(categoryController.getCategoriesWithTransport());
+
+        System.out.println("Enter from City");
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        String fromCityName = reader.readLine();
+
+
+        System.out.println("Enter to City");
+        reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        String toCity = reader.readLine();
+
+        System.out.println(fromCityName);
+        System.out.println(toCity);
+
+        System.out.println("Number of people");
+        reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        String peopleAmount = reader.readLine();
+
+        System.out.println("Cargo Amount");
+        reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        String cargoAmount = reader.readLine();
+
+        CitiesController citiesController = new CitiesController();
+
+        City cityFrom = citiesController.getCityByName(fromCityName);
+        City cityTo = citiesController.getCityByName(toCity);
+
+        TransportController transportController = new TransportController();
+        transportController.checkMatch(cityFrom, cityTo, peopleAmount, cargoAmount);
+
+
+        System.out.println("Git git");
+    }
+}
+
+
